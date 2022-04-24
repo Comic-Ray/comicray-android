75cc61afd4f3efb975d31e8240adaa99c116b52e
Kaustubh Patange <developerkp16@gmail.com>
2022-04-25 00:04:26 +0530

    revamp detail screen

--- app/src/main/java/com/comicreader/comicray/data/models/comicDetails/Genre.kt
+++
@@ -1,6 +0,0 @@
-package com.comicreader.comicray.data.models.comicDetails
-
-data class Genre(
-    val name: String,
-    val tag: String
-)
--- app/src/main/java/com/comicreader/comicray/data/models/mangaDetails/Genre.kt
+++
@@ -1,6 +0,0 @@
-package com.comicreader.comicray.data.models.mangaDetails
-
-data class Genre(
-    val category: String,
-    val name: String
-)
